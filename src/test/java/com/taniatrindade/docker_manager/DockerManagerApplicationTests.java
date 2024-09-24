046b3c7234e16231b6cb07678c255894848eb352
Tânia Trindade <flaviatna@gmai.com>
2024-09-24 18:17:26 -0300

    Initial commit

---
+++ src/test/java/com/taniatrindade/docker_manager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taniatrindade.docker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
